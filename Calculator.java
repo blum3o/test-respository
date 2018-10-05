b22c4804475d33cc91e969d1b84b8df0b891639c
dev-jaimie <dev-jaimie@gmail.com>
2018-10-05 14:38:37 +0900

    multiply, div 메소드 추가

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,10 @@ public class Calculator{
     {
         return a-b;
     }
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+    public static float div(int a, int b) {
+        return a/b;
+    }
 }
